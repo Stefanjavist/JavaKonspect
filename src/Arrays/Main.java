c65f56d0f628a542f23309a1138c19ca5e982eeb
Stefan <s.chagay@mail.ru>
2017-06-30 21:40:47 +0300

    add new practice

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -12,13 +12,14 @@ public class Main {
         MultiArrayString mulArrStr = new MultiArrayString();
         mulArrStr.iMultiArrayString();
 
-        /**foreach
+        /**
+         * foreach
          * Он позволяет более просто записать проход по всем элементам массива.
          */
         int[] sample = {12, 32, 23, 4, 53, 11, 33,};
 
         //выводим элементы в цикле foreach
-        for (int t : sample){
+        for (int t : sample) {
             System.out.println(t);
         }/*
         Эту запись можно интепретировать следующим образом — цикл проходит по всем элементам массива,
@@ -30,41 +31,43 @@ public class Main {
          * перменная t и переменная sample[0] — это РАЗНЫЕ переменные. Рассмотрим более сложный пример:
          */
 
-        int [] sample1 = new int[5];
+        int[] sample1 = new int[5];
 
         System.out.println("До цикла foreach");
-        for (int i : sample1){
+        for (int i : sample1) {
 
         }
 
         //Думаем, что происходит инициализация
-        for (int i : sample1){
+        for (int i : sample1) {
             i = 99;
         }
 
         System.out.println("После foreach");
-        for (int i : sample1){
+        for (int i : sample1) {
             System.out.println(i);
         }
 //~~~~~~~~~~~~~~~~~~~~~~~~~~
         /**
          * Для инициализации придется использовать обычную конструкцию.
          */
-        int [] sample2 = new int[5];
+        int[] sample2 = new int[5];
 
         System.out.println("До цикла foreach");
-        for (int i : sample2){
+        for (int i : sample2) {
 
         }
 
         //Думаем, что происходит инициализация
-        for (int i = 0; i<sample2.length; i++){
+        for (int i = 0; i < sample2.length; i++) {
             sample2[i] = 99;
         }
 
         System.out.println("После foreach");
-        for (int i : sample2){
+        for (int i : sample2) {
             System.out.println(i);
         }
     }
 }
+
+
