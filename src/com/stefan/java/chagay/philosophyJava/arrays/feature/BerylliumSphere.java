c31c3d72d6c82bed726abf88f47f4acfacdce669
Stefan <s.chagay@mail.ru>
2017-07-23 22:26:40 +0300

    философия java arrays

---
+++ src/com/stefan/java/chagay/philosophyJava/arrays/feature/BerylliumSphere.java
@@ -0,0 +1,13 @@
+package com.stefan.java.chagay.philosophyJava.arrays.feature;
+
+
+public class BerylliumSphere {
+    
+    private static long counter;
+    private final long id = counter++;
+
+    public String toString() {
+        return "Sphere" + id;
+    }
+    
+}
---
+++ src/com/stefan/java/chagay/philosophyJava/arrays/feature/ContainerComparison.java
@@ -0,0 +1,43 @@
+package com.stefan.java.chagay.philosophyJava.arrays.feature;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ContainerComparison {
+
+    /**
+     * в следующем примере сравниваются массивы и параметризованные контейнеры
+     * @param args
+     */
+    public static void main(String[] args) {
+        BerylliumSphere[] spheres = new BerylliumSphere[10];
+        for (int i = 0; i < 5; i++) {
+            spheres[i] = new BerylliumSphere();
+        }
+        System.out.println(Arrays.toString(spheres));
+        System.out.println(spheres[4]);
+//------------------------------------------
+        List<BerylliumSphere> sphereList = new ArrayList<BerylliumSphere>();
+        for (int i = 0; i < 5; i++) {
+            sphereList.add(new BerylliumSphere());
+        }
+        System.out.println(sphereList);
+        System.out.println(sphereList.get(4));
+
+        //--------------------------
+        int[] i = {0, 1, 2, 3, 4, 5, 6};
+        System.out.println(Arrays.toString(i));
+        System.out.println(i[4]);
+
+
+        List<Integer> listInt = new ArrayList<Integer>(
+        Arrays.asList(0, 1, 2, 3, 4, 5));
+
+        listInt.add(97);
+        System.out.println(listInt);
+        System.out.println(listInt.get(4));
+    }
+
+}
