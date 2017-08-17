b864e45f1faeaec20090da8e0e1325e45e027700
Stefan <s.chagay@mail.ru>
2017-08-17 21:40:24 +0300

    ASiCOM

--- src/com/stefan/java/chagay/computer_sience/Algorithms/AsimtotComplexity.java
+++ src/com/stefan/java/chagay/computer_sience/Algorithms/AsimtotComplexity.java
@@ -4,7 +4,7 @@ package com.stefan.java.chagay.computer_sience.Algorithms;
 public class AsimtotComplexity {
 
     public static void main(String[] args) {
-        int[] art = {23, 324, 34343, 3, 35, 1212, 124};
+        int[] art = {23, 324, 3, 3, 35, 1212, 124};
 
         System.out.println(findLargest(art));
 
@@ -13,7 +13,7 @@ public class AsimtotComplexity {
     //произыодитеьность этого алгоритма составляет O(N)
     public static int findLargest(int[] art) {
         int largest = art[0];
-        for (int i = 1; i < 7; i++)
+        for (int i = 1; i < art.length; i++)
             if (art[i] > largest) {
                 largest = art[i];
             }
